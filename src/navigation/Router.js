ca408a3f9772a922efb46259a373e270640464ab
Sue Ceklosky <cekloskys@chc.edu>
2024-02-04 13:20:29 -0500

    Added Home Screen

--- src/navigation/Router.js
+++ src/navigation/Router.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import {NavigationContainer} from '@react-navigation/native';
+import HomeScreen from '../screens/Home';
 import TabNavigator from './TabNavigator';
 import AddHostScreen from '../screens/AddHost';
 
@@ -10,11 +11,12 @@ const Router = props => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen
-          name={'Start Scheduling!'}
-          component={TabNavigator}
+      <Stack.Screen
+          name={'Home'}
+          component={HomeScreen}
           options={{headerShown: false}}
         />
+        <Stack.Screen name={'Start Scheduling!'} component={TabNavigator}/>
         <Stack.Screen name={'Add Host'} component={AddHostScreen}/>
       </Stack.Navigator>
     </NavigationContainer>
